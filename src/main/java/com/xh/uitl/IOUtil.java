7ed4f0c8b347ae499506dc172e1b6419e6221e04
陈专懂 <czdong@x.xinhai.com>
2018-06-19 17:06:58 +0800

    新增 新增预览、下载功能

--- src/main/java/com/xh/uitl/IOUtil.java
+++ src/main/java/com/xh/uitl/IOUtil.java
@@ -1,11 +1,20 @@
 package com.xh.uitl;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
@@ -17,7 +26,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.context.ContextLoader;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.xh.base.Constant;
 
 public final class IOUtil {
 	private static final Logger log = LoggerFactory.getLogger(IOUtil.class);
@@ -40,7 +48,7 @@ public final class IOUtil {
 		if (!mf.isEmpty()) {
 			String oldFileName = mf.getOriginalFilename();
 			String suffix = oldFileName.substring(oldFileName.lastIndexOf("."));
-			System.err.println("suffix:"+suffix);
+//			System.err.println("suffix:"+suffix);
 			switch (suffix) {
 			case ".xls":
 			case ".xlsx":
@@ -82,10 +90,11 @@ public final class IOUtil {
 	 * @return: ResponseEntity<byte[]>
 	 *
 	 */
-	public static final ResponseEntity<byte[]> downloadFile(String path, String fileName) {
+	public static final ResponseEntity<byte[]>  downloadFile(String path, String fileName) {
 		// 下载文件路径
 		// String path = request.getServletContext().getRealPath("/images/");
 		File file = new File(realPath + File.separator + path + File.separator + fileName);
+		System.err.println("IOUtil:"+file);
 		HttpHeaders headers = new HttpHeaders();
 		// 下载显示的文件名，解决中文名称乱码问题
 		try {
@@ -103,6 +112,44 @@ public final class IOUtil {
 			log.error("IO工具类【downloadFile】方法异常,异常原因:" + e.getMessage());
 			return null;
 		}
+	}
+	/**
+	 * 
+	 * @Title: downloadFileTrue  
+	 * @Description: TODO(这里用一句话描述这个方法的作用)  
+	 * @author 陈专懂 
+	 * @return HttpServletResponse 
+	 * @date 2018年6月19日  
+	 * @version 1.0
+	 */
+	public static final HttpServletResponse  downloadFileTrue(String path, String fileName,HttpServletResponse resp) {
+		 try {
+	            // path是指欲下载的文件的路径。
+	            File file = new File(path);
+	            // 取得文件名。
+	            String filename = file.getName();
+	            // 取得文件的后缀名。
+	            String ext = filename.substring(filename.lastIndexOf(".") + 1).toUpperCase();
+	            // 以流的形式下载文件。
+	            InputStream fis = new BufferedInputStream(new FileInputStream(path));
+	            byte[] buffer = new byte[fis.available()];
+	            fis.read(buffer);
+	            fis.close();
+	            // 清空response
+	            resp.reset();
+	            // 设置response的Header
+	            resp.addHeader("Content-Disposition", "attachment;filename=" + new String(filename.getBytes()));
+	            resp.addHeader("Content-Length", "" + file.length());
+	            OutputStream toClient = new BufferedOutputStream(resp.getOutputStream());
+	            resp.setContentType("application/octet-stream");
+	            toClient.write(buffer);
+	            toClient.flush();
+	            toClient.close();
+	        } catch (IOException ex) {
+				log.error("IO工具类【downloadFile】方法异常,异常原因:" + ex.getMessage());
+				return null;
+	        }
+	        return resp;
 	}
 
 	/**
@@ -168,4 +215,31 @@ public final class IOUtil {
 			return null;
 		}
 	}
+	
+	/**
+	 * 
+	 * @Title: displayPDF  
+	 * @Description: TODO(pdf.js)  
+	 * @author 陈专懂 
+	 * @return void 
+	 * @date 2018年6月19日  
+	 * @version 1.0
+	 */
+	public void displayPDF(HttpServletResponse response,HttpServletRequest request,String fileAddress) {
+        try {
+            File file = new File(fileAddress);
+            FileInputStream fileInputStream = new FileInputStream(file);
+            byte[] b=new byte[fileInputStream.available()];
+            fileInputStream.read(b);
+            response.setHeader("Content-Disposition", "attachment;fileName=test.pdf");
+            ServletOutputStream out=response.getOutputStream();
+            out.write(b);
+            out.flush();
+            out.close();
+            
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+	
 }
