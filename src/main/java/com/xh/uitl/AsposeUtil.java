87891eb83c669b05dcae1c01935eff0e749a0b4c
黄官易 <hgy@x.xinhai.com>
2018-06-20 16:43:01 +0800

    新增 新增AsposeUtil增加Excel转换PDF

--- src/main/java/com/xh/uitl/AsposeUtil.java
+++ src/main/java/com/xh/uitl/AsposeUtil.java
@@ -2,6 +2,7 @@ package com.xh.uitl;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -9,6 +10,7 @@ import org.apache.ibatis.io.Resources;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.aspose.cells.Workbook;
 import com.aspose.words.Document;
 import com.aspose.words.License;
 import com.aspose.words.SaveFormat;
@@ -19,7 +21,7 @@ public class AsposeUtil {
 	/**
 	 * 
 	 * @Title: getLicenseWithWord
-	 * @Description: 获得许可证
+	 * @Description: 获得许可证word
 	 * @author 黄官易
 	 * @return
 	 * @return boolean
@@ -187,8 +189,55 @@ public class AsposeUtil {
 		}
 		return pdfFileIs;
 	}
-	
-	
+
+	/**
+	 * 
+	 * @Title: getLicenseWithExcel
+	 * @Description: 获得许可证Excel
+	 * @author 黄官易
+	 * @return
+	 * @return boolean
+	 * @date 2018年6月20日
+	 * @version 1.0
+	 */
+	public static boolean getLicenseWithExcel() {
+		boolean result = false;
+		try {
+			InputStream is = Resources.getResourceAsStream("license/license_word.xml");
+			License aposeLic = new License();
+			System.err.println(aposeLic);
+			aposeLic.setLicense(is);
+			result = true;
+		} catch (Exception e) {
+			log.error("加载excel许可失败,失败原因：【" + e.toString() + "】");
+		}
+		return result;
+	}
+
+	public static String excel2PDFStr(String filePath) {
+		String pdfPath = "";
+		try {
+//			if (getLicenseWithExcel()) {
+//				File file = new File(filePath);
+//				FileInputStream is = new FileInputStream(file);
+				Workbook wb = new Workbook("D://111.xlsx");
+				pdfPath = "src//main//pdf//" + DateUtil.curDateYMDHMSSForService() + ".pdf";
+				File file = new File(pdfPath);
+				FileOutputStream os = new FileOutputStream(file);
+				wb.save(os, SaveFormat.PDF);
+//			}
+		} catch (Exception e) {
+
+		} finally {
+		}
+
+		return pdfPath;
+	}
+
+	public static String excel2PDFStr(InputStream is) {
+
+		return "";
+	}
 
 	public static void main(String[] arg) throws IOException {
 		// D:\\123测试.docx
@@ -202,6 +251,9 @@ public class AsposeUtil {
 		// InputStream word2pdfStream = word2PDFStream(new
 		// FileInputStream("D:\\123xin是.doc"));
 		// System.err.println(word2pdfStream);
+
+		String excel2pdfStr = excel2PDFStr("D://111.xlsx");
+		System.err.println(excel2pdfStr);
 
 	}
 }
