83144e1eb7df0e400db2848508ca9f2498b1f998
陈专懂 <czdong@x.xinhai.com>
2018-06-22 17:15:31 +0800

    新增  新增项目模块（添加项目基础表、项目用户关联表）

--- src/main/java/com/xh/service/IProjectService.java
+++ src/main/java/com/xh/service/IProjectService.java
@@ -3,7 +3,11 @@
  */
 package com.xh.service;
 
-import com.xh.entity.KbFileTable;
+
+import java.util.List;
+import java.util.Map;
+
+
 import com.xh.entity.KbProjectTable;
 import com.xh.uitl.Result;
 
@@ -16,5 +20,11 @@ public interface IProjectService {
 	//动态建表
 	public Result<Object> createProjectList(KbProjectTable kbpt);
 
-	public Result<Object> inseFileTable(KbFileTable kft) throws Exception;
+	//根据项目等级查询上级项目的编号、名字
+	public Result<Object> selectProjectTableNameByProjectLevel(int projectLevel);
+	//添加项目信息
+	public Result<Object> insertProject(Map<String,Object> map);
+	//根据员工的编号，查找员工信息
+	public Result<Object> selectUserByUserCode(List<String> strList,Map<String,Object> map,String projectPermission);
+	
 }
