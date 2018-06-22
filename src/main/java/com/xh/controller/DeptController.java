3e47ef4d22e024d139710fd729e83c34909f5ca4
黄官易 <hguanyi@myxinhai.cn>
2018-06-22 21:39:11 +0800

    新增 新增部门方法管理入口

---
+++ src/main/java/com/xh/controller/DeptController.java
@@ -0,0 +1,12 @@
+package com.xh.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.xh.base.BaseController;
+
+@Controller
+@RequestMapping("/dept")
+public class DeptController extends BaseController {
+
+}
