94d14c7170f84a1493f29c62e1d6548b13dba868
黄官易 <hgy@x.xinhai.com>
2018-07-13 11:24:01 +0800

    优化 优化 项目部分页面跳转接口与错误页面整合

--- src/main/java/com/xh/controller/IndexController.java
+++ src/main/java/com/xh/controller/IndexController.java
@@ -31,30 +31,6 @@ public class IndexController extends BaseController{
 	@Autowired
 	@Qualifier("userAdviceServiceImpl")
 	private IUserAdviceService ads;
-	/**
-	 * 
-	 * @Title: office2Pdf  
-	 * @Description: 转换pdfdemo 
-	 * @author 黄官易
-	 * @param request
-	 * @param response
-	 * @return    
-	 * @return ResponseEntity<byte[]> 
-	 * @date 2018年6月21日  
-	 * @version 1.0
-	 */
-	@RequestMapping("/pdf2.do")
-	@ResponseBody
-	public ResponseEntity<byte[]> office2Pdf(HttpServletRequest request, HttpServletResponse response) {
-		String realPath = ContextLoader.getCurrentWebApplicationContext().getServletContext().getRealPath("/");
-		System.err.println(realPath);
-		String word2pdfStr = AsposeUtil.word2PDFStr(realPath + "..//upload//123xin是.docx");
-		System.err.println(word2pdfStr);
-		String substring = word2pdfStr.substring(word2pdfStr.lastIndexOf("/")+1, word2pdfStr.length());
-		System.err.println(substring);
-		ResponseEntity<byte[]> downloadFile = IOUtil.downloadFile("pdf", substring);
-		return downloadFile;
-	}
 	
 	@RequestMapping("/getAllMsg.do")
 	@ResponseBody
