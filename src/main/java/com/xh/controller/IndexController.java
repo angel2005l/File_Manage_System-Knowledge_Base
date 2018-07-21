6160198624165bc52f4cc78f097e37e7b5cbce18
黄官易 <hgy@x.xinhai.com>
2018-07-21 13:21:10 +0800

    优化 优化消息通知

--- src/main/java/com/xh/controller/IndexController.java
+++ src/main/java/com/xh/controller/IndexController.java
@@ -10,18 +10,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.context.ContextLoader;
 
 import com.alibaba.fastjson.JSONObject;
 import com.xh.base.BaseController;
 import com.xh.entity.KbUserAdvice;
 import com.xh.service.IUserAdviceService;
-import com.xh.uitl.AsposeUtil;
-import com.xh.uitl.IOUtil;
 import com.xh.uitl.Result;
 
 @Controller
